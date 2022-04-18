412689339416be0db8d868c5d24f35604ac53373
gevdneyn <gerwoud.vandeneynden@ugent.be>
2022-04-18 16:07:53 +0200

    eerste prototype voor groepsscherm

--- HeppiHome/app/src/main/java/com/heppihome/ui/groupscreen/GroupScreen.kt
+++ HeppiHome/app/src/main/java/com/heppihome/ui/groupscreen/GroupScreen.kt
@@ -24,6 +24,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+/*
+class for graphical part of group selection scree
+ */
+
 class GroupScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
