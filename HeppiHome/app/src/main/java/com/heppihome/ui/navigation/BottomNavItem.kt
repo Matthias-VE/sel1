695d290dfcaf9e92b0111488d4cb2ff81dd42993
Alexandre Paice <alexandre.paice@ugent.be>
2022-04-20 16:37:10 +0200

    bottomnavitem

--- HeppiHome/app/src/main/java/com/heppihome/ui/navigation/BottomNavItem.kt
+++ HeppiHome/app/src/main/java/com/heppihome/ui/navigation/BottomNavItem.kt
@@ -1,8 +1,7 @@
-import compose.R
 
 sealed class BottomNavItem(var title: String, var icon:Int, var screen_route:String){
 
-    object Tasks: BottomNavItem("Tasks", R.drawable.ic_job, "tasks")
-    object Overview: BottomNavItem("Overview", R.drawable.ic_menu_my_calendar, "calendar")
-    object Settings: BottomNavItem("Settings", R.drawable.ic_menu_preferences, "settings")
+    object Tasks: BottomNavItem("Tasks", android.R.drawable.ic_menu_edit, "tasks")
+    object Overview: BottomNavItem("Overview", android.R.drawable.ic_menu_my_calendar, "calendar")
+    object Settings: BottomNavItem("Settings", android.R.drawable.ic_menu_preferences, "settings")
 }
