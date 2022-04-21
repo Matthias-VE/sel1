d40b2c01836298560e6ac1bf7e36d620203ef668
gevdneyn <gerwoud.vandeneynden@ugent.be>
2022-04-21 19:52:44 +0200

    files added

---
+++ HeppiHome/app/src/main/java/com/heppihome/ui/navigation/BottomNavItem.kt
@@ -0,0 +1,7 @@
+package com.heppihome.ui.navigation
+sealed class BottomNavItem(var title: String, var icon:Int, var screen_route:String){
+
+    object Tasks: BottomNavItem("Tasks", android.R.drawable.ic_menu_edit, "tasks")
+    object Overview: BottomNavItem("Overview", android.R.drawable.ic_menu_my_calendar, "calendar")
+    object Settings: BottomNavItem("Settings", android.R.drawable.ic_menu_preferences, "settings")
+}
