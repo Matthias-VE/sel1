71a3ede3daac550615d04f8c5fd0526cac18cf74
gevdneyn <gerwoud.vandeneynden@ugent.be>
2022-04-29 13:37:51 +0200

    laatste cahnges voor merge

---
+++ HeppiHome/app/src/main/java/com/heppihome/ui/components/NewGroup.kt
@@ -0,0 +1,74 @@
+package com.heppihome.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.heppihome.ui.routes.DropdownIcon
+import com.heppihome.viewmodels.AddGroupViewModel
+
+@Composable
+fun NewGroup(
+    vM : AddGroupViewModel,
+    onGroupCancel : () -> Unit
+) {
+    val temp by vM.groupName.collectAsState()
+    val temp2 by vM.description.collectAsState()
+
+    Column() {
+        Header(onGroupCancel)
+        Column(modifier = Modifier
+            .padding(10.dp)) {
+            InputField(name = "Name Of Group", description = temp.text, vM, {x -> vM.setGroup(x)})
+            InputField(name = "Description", description = temp2.text, vM, {x -> vM.setDescription(x)})
+            Button(onClick = { vM.addGroups() },
+                modifier = Modifier.padding(10.dp)) {
+                Text("Add")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Header(onGroupCancel: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colors.primary) {
+        Row() {
+            Row(modifier = Modifier
+                .padding(10.dp), horizontalArrangement = Arrangement.Start) {
+                IconButton(onClick = onGroupCancel) {
+                    Icon(
+                        Icons.Default.Close, contentDescription = "Cancel", modifier = Modifier
+                            .size(40.dp)
+                    )
+                }
+            }
+            Row(modifier = Modifier.padding(10.dp), horizontalArrangement = Arrangement.Center) {
+                Text("New Group", fontSize = 30.sp, textAlign = TextAlign.Center)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun InputField(name : String, description : String, vM: AddGroupViewModel, edit : (String) -> Unit) {
+
+    Column(modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+        Text(name, color = Color.Gray)
+        OutlinedTextField(value = description, modifier = Modifier.fillMaxWidth(), onValueChange = { newText ->
+            edit(newText)
+        })
+    }
+}
---
+++ HeppiHome/app/src/main/java/com/heppihome/viewmodels/AddGroupViewModel.kt
@@ -0,0 +1,47 @@
+package com.heppihome.viewmodels
+
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.heppihome.data.HomeRepository
+import com.heppihome.data.models.Group
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddGroupViewModel @Inject constructor(private val rep : HomeRepository) : ViewModel() {
+
+    private var _groupName : MutableStateFlow<TextFieldValue> = MutableStateFlow(TextFieldValue(""))
+    var groupName : StateFlow<TextFieldValue> = _groupName
+
+    private var _description : MutableStateFlow<TextFieldValue> = MutableStateFlow(TextFieldValue(""))
+    var description : StateFlow<TextFieldValue> = _description
+
+    fun setGroup(newName : String) {
+        _groupName.value = TextFieldValue(newName)
+    }
+
+    fun setDescription(newDes : String) {
+        _description.value = TextFieldValue(newDes)
+    }
+
+    fun addGroups() {
+        viewModelScope.launch {
+            val toAdd = Group(_groupName.value.text, _description.value.text, emptyList())
+            //println(_groupName.value.text)
+            rep.addGroup(toAdd).collect()
+            setGroup("")
+            setDescription("")
+
+        }
+    }
+
+
+
+}
