ce743d0b240831144ea4627f4d255c6d1c8fb88e
Arnout_Allaert <arnout.allaert@ugent.be>
2022-05-16 22:19:40 +0200

    normaal alle strings

--- HeppiHome/app/src/main/java/com/heppihome/ui/navigation/BottomNavItem.kt
+++ HeppiHome/app/src/main/java/com/heppihome/ui/navigation/BottomNavItem.kt
@@ -6,11 +6,12 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.heppihome.R
 
-sealed class BottomNavItem(var title: String, var icon: ImageVector, var screen_route:String){
+sealed class BottomNavItem(var title: Int, var icon: ImageVector, var screen_route:String){
 
-    object Tasks: BottomNavItem("Tasks", Icons.Filled.Edit, HomeAppDestinations.TASKS_ROUTE)
-    object Overview: BottomNavItem("Overview", Icons.Filled.DateRange, HomeAppDestinations.CALENDAR)
-    object Shop : BottomNavItem("Shop", Icons.Filled.ShoppingCart, HomeAppDestinations.SHOP_ROUTE)
-    object Settings: BottomNavItem("Settings", Icons.Filled.Settings, HomeAppDestinations.SETTINGS_ROUTE)
+    object Tasks: BottomNavItem(R.string.Tasks, Icons.Filled.Edit, HomeAppDestinations.TASKS_ROUTE)
+    object Overview: BottomNavItem(R.string.Overview, Icons.Filled.DateRange, HomeAppDestinations.CALENDAR)
+    object Shop : BottomNavItem(R.string.Shop, Icons.Filled.ShoppingCart, HomeAppDestinations.SHOP_ROUTE)
+    object Settings: BottomNavItem(R.string.Settings, Icons.Filled.Settings, HomeAppDestinations.SETTINGS_ROUTE)
 }
